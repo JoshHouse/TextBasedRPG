174033afb14aa62051aa867250641e99c98d9450
Cameron Nickerson <nickersonc20@students.ecu.edu>
2024-10-17 22:05:12 +0000

    Upload KeyItem.java

---
+++ src/game/KeyItem.java
@@ -0,0 +1,23 @@
+package game;
+
+public class KeyItem extends Item {
+
+	/**
+	 * Base constructor
+	 */
+	public KeyItem() {
+		super();
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @param name
+	 * @param info
+	 * @param rarity
+	 */
+	public KeyItem(String key, String name, String info, int rarity) {
+		super(key, name, info, rarity);
+	}
+
+}
