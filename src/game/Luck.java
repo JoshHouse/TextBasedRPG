5e227c3a596c311c44ea62aadf363e579896e2c4
Cameron Nickerson <nickersonc20@students.ecu.edu>
2024-10-17 22:12:39 +0000

    Upload Luck.java

---
+++ src/game/Luck.java
@@ -0,0 +1,37 @@
+package game;
+
+import java.util.Random;
+
+public class Luck {
+
+	/**
+	 * Set the max value to 101 because the counting for random numbers staets at 0.
+	 * Setting it to 101 tells the random variable to get a number between 0 and
+	 * 100, which is a tital of 101 possible choices
+	 */
+	private static final int MAX = 101;
+
+	/**
+	 * Performs a luck based event by calculating a random number between 0 and 100,
+	 * then comparing it to the luck variable provided in its input.
+	 * 
+	 * @param baseLuck - The user's base luck status.
+	 * @return If the generated number is lower or equal to the provided luck
+	 *         integer, return true. If it is greater, return false
+	 */
+	public static boolean luckEvent(int baseLuck) {
+
+		Random rand = new Random(); // Create a random object.
+
+		int luckyNumber = rand.nextInt(MAX); // Generates a random number between 0 and 100.
+
+		// Determine which is higher, baseLuck or luckyNumber.
+		if (baseLuck >= luckyNumber) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+}
