626cd7c5cf51ce9d2337ad18b08863ed126ebdb6
Cameron Nickerson <nickersonc20@students.ecu.edu>
2024-10-28 01:16:56 +0000

    Made improvements to the leveling up process, and gave the player the option to choose whether they want to use the skill points they've earned or not.

--- src/game/Player.java
+++ src/game/Player.java
@@ -6,14 +6,15 @@ public class Player extends GameChar {
 
 	// -------------------Variables-------------------
 
-	protected Weapon equipped;
-	protected int mana;
-	protected double expThreshold;
-	protected int lvlMelee;
-	protected int lvlRanged;
-	protected int lvlMage;
-	protected int lvlRogue;
-	protected int lvlLuck;
+	private Weapon equipped;
+	private int mana;
+	private double expThreshold;
+	private int lvlMelee;
+	private int lvlRanged;
+	private int lvlMage;
+	private int lvlRogue;
+	private int lvlLuck;
+	private int sklPt = 4;
 
 	// -------------------Constructors-------------------
 
@@ -49,10 +50,10 @@ public class Player extends GameChar {
 	 * @param inputlvlRogue
 	 * @param inputlvlLuck
 	 */
-	public Player(String inputName, Inventory inputInventory, int inputCurrency, int inputHealth, 
-			int inputLevel, int inputExp, Weapon inputEquipped, int inputMana, int inputlvlMelee,
-			int inputlvlRanged, int inputlvlMage, int inputlvlRogue, int inputlvlLuck) {
-		
+	public Player(String inputName, Inventory inputInventory, int inputCurrency, int inputHealth, int inputLevel,
+			int inputExp, Weapon inputEquipped, int inputMana, int inputlvlMelee, int inputlvlRanged, int inputlvlMage,
+			int inputlvlRogue, int inputlvlLuck) {
+
 		super(inputName, inputInventory, inputCurrency, inputHealth, inputLevel, inputExp);
 		this.equipped = inputEquipped;
 		this.mana = inputMana;
@@ -66,7 +67,7 @@ public class Player extends GameChar {
 		this.lvlMage = inputlvlMage;
 		this.lvlLuck = inputlvlLuck;
 	}
-	
+
 	/**
 	 * For creating a level 1 player only taking a name as input.
 	 * 
@@ -82,7 +83,7 @@ public class Player extends GameChar {
 		this.lvlMage = 1;
 		this.lvlRogue = 1;
 		this.lvlLuck = 1;
-		
+
 	}
 
 	// -------------------Getters-------------------
@@ -90,15 +91,15 @@ public class Player extends GameChar {
 	public Weapon getEquipped() {
 		return this.equipped;
 	}
-	
+
 	public int getMana() {
 		return this.mana;
 	}
-	
+
 	public double getExpThreshold() {
 		return this.expThreshold;
 	}
-	
+
 	public int getlvlMelee() {
 		return this.lvlMelee;
 	}
@@ -110,7 +111,7 @@ public class Player extends GameChar {
 	public int getlvlMage() {
 		return this.lvlMage;
 	}
-	
+
 	public int getlvlRogue() {
 		return this.lvlRogue;
 	}
@@ -119,38 +120,46 @@ public class Player extends GameChar {
 		return this.lvlLuck;
 	}
 
+	public int getSklPt() {
+		return this.sklPt;
+	}
+
 	// -------------------Setters-------------------
 
-	public void setEquipped(Weapon inputEquipped) {
-		this.equipped = inputEquipped;
-	}
-	
-	public void setMana(int inputMana) {
-		this.mana = inputMana;
-	}
-	
-	public void setExpThreshold(Double inputExpThreshold) {
-		this.expThreshold = inputExpThreshold;
-	}
-	
-	public void setlvlMelee(int inputlvlMelee) {
-		this.lvlMelee = inputlvlMelee;
+	public void setEquipped(Weapon equipped) {
+		this.equipped = equipped;
 	}
 
-	public void setlvlRanged(int inputlvlRanged) {
-		this.lvlRanged = inputlvlRanged;
+	public void setMana(int mana) {
+		this.mana = mana;
 	}
 
-	public void setlvlMage(int inputlvlMage) {
-		this.lvlMage = inputlvlMage;
-	}
-	
-	public void setlvlRogue(int inputlvlRogue) {
-		this.lvlRogue = inputlvlRogue;
+	public void setExpThreshold(Double expThreshold) {
+		this.expThreshold = expThreshold;
 	}
 
-	public void setlvlLuck(int inputlvlLuck) {
-		this.lvlLuck = inputlvlLuck;
+	public void setlvlMelee(int lvlMelee) {
+		this.lvlMelee = lvlMelee;
+	}
+
+	public void setlvlRanged(int lvlRanged) {
+		this.lvlRanged = lvlRanged;
+	}
+
+	public void setlvlMage(int lvlMage) {
+		this.lvlMage = lvlMage;
+	}
+
+	public void setlvlRogue(int lvlRogue) {
+		this.lvlRogue = lvlRogue;
+	}
+
+	public void setlvlLuck(int lvlLuck) {
+		this.lvlLuck = lvlLuck;
+	}
+
+	public void setSklPt(int sklPt) {
+		this.sklPt = sklPt;
 	}
 
 	// -------------------Functions-------------------
@@ -167,25 +176,19 @@ public class Player extends GameChar {
 	 */
 	public void profile() {
 
-		System.out.println("-----------------------YOUR PROFILE-----------------------------\n" + 
-							"Name: " + this.name + "\n" + 
-							"Level: " + this.level + "\n" +
-							"EXP till next level: " + (int) (this.expThreshold - this.exp) + "\n" +
-							"Max HP: " + this.health + "\n" + 
-							"Mana: " + this.mana + "\n" + 
-							"Luck: " + this.lvlLuck + "\n" + 
-							"Money: "+ this.currency + "\n" + 
-							"-----Levels-----\n" +
-							"Melee Lv: " + this.lvlMelee + "\n" + 
-							"Ranged Lv: " + this.lvlRanged + "\n" + 
-							"Rouge Lv: " + this.lvlRogue + "\n" + 
-							"Mage Lv: " + this.lvlMage + "\n" +
-							"----------------------------------------------------------------\n");
+		System.out.println("-----------------------YOUR PROFILE-----------------------------\n" + "Name: " + this.name
+				+ "\n" + "Level: " + this.level + "\n" + "EXP till next level: " + (int) (this.expThreshold - this.exp)
+				+ "\n" + "Max HP: " + this.health + "\t\t" + "Mana: " + this.mana + "\n" + "Luck: " + this.lvlLuck
+				+ "\t\t" + "Money: " + this.currency + "\n" + "\t\t\t-----Levels-----\n" + "Melee Lv: " + this.lvlMelee
+				+ "\t" + "Ranged Lv: " + this.lvlRanged + "\t" + "Rouge Lv: " + this.lvlRogue + "\t" + "Mage Lv: "
+				+ this.lvlMage + "\n" + "----------------------------------------------------------------\n");
 
 	}
 
 	/**
-	 * Changes the mainhand weapon of the player, based on the weapons in their inventory.
+	 * Changes the mainhand weapon of the player, based on the weapons in their
+	 * inventory.
+	 * 
 	 * @param index - The index of the weapon in inventory.
 	 */
 	public void selectMainhandWeapon(int index) {
@@ -197,98 +200,111 @@ public class Player extends GameChar {
 	}
 
 	/**
-	 * Function to level up the player. Takes the overflow EXP amount as an int
-	 * value. Sets EXP to overflow amount, increments the level of the player,
-	 * increases the EXPThreshold, and prompts the player to upgrade a class level
-	 * with the skill point gained by the level up
+	 * A void method that allows the player to spend skill points to upgrade their
+	 * various stats. Each level up lets the player earn a skill point, which can be
+	 * spent here to upgrade stats, one point at a time. However, the method exits
+	 * immediately if the player runs out of skill points, or they attempt to spend
+	 * non-existent skill points.
 	 * 
-	 * @param Overflow
+	 * @param scn - The scanner object. Used for handling user input within the
+	 *            method.
 	 */
-	public void levelUp(int overflow) {
+	public void increaseStats(Scanner scn) {
 
-		this.level = this.level + 1;
-		this.exp = overflow;
-		this.expThreshold = this.expThreshold * 1.2;
-		Boolean SkillPointUsed = false; // Used to ensure player doesn't waste their skill point
-		Scanner Keyboard = new Scanner(System.in);
+		String input; // Holds user input
 
-		// Original prompt for the player to choose what level they would like to
-		// upgrade
-		System.out.println("You have leveled up! What would you like to use your skillpoint on? \n"
-				+ "1) Melee Level: Your current Melee Level is " + this.lvlMelee + "\n"
-				+ "2) Ranged Level: Your current Ranged Level is " + this.lvlRanged + "\n"
-				+ "3) Rogue Level: Your current Rogue Level is " + this.lvlRogue + "\n"
-				+ "4) Mage Level: Your current Mage Level is " + this.lvlMage + "\n"
-				+ "5) Luck Level: Your current Luck Level is " + this.lvlLuck + "\n"
-				+ "Tip) To choose what you would like to level up, type either the number or class. Ex. '1' or 'melee'");
+		if (this.sklPt <= 0) { // Exit the method if it is called when there's no skill points
+			System.out.println("You don't have any skill points to use.");
+			return;
+		}
 
-		// While loop to ensure the player uses their skill point
-		while (SkillPointUsed == false) {
+		while (this.sklPt > 0) { // Allow user to keep spending till their skill points are gone, or they cancel
 
-			String choice = Keyboard.nextLine();
-			choice.toLowerCase(); // ensures choice is not case sensitive
+			// Menu
+			System.out.println("-----------------------------------");
+			System.out.println("Available Skill Points: " + this.sklPt);
+			System.out.println("Which stat do you want to level up?\n");
+			System.out.println("1) Melee (Lvl: " + this.lvlMelee + ")");
+			System.out.println("2) Ranged (Lvl: " + this.lvlRanged + ")");
+			System.out.println("3) Rogue (Lvl: " + this.lvlRogue + ")");
+			System.out.println("4) Mage (Lvl: " + this.lvlMage + ")");
+			System.out.println("5) Luck (Lvl: " + this.lvlLuck + ")");
+			System.out.println("6) Cancel\n");
+			System.out.println("-----------------------------------");
 
-			// Based on choice, increments the chosen level and prints a message with their
-			// updated level. Sets
-			// SkillPointUsed to true if a class level is upgraded
-			switch (choice) {
-			case "1", "melee": {
+			System.out.print("Type the number or the name of the stat: ");
+			input = scn.nextLine();
+			input = input.toLowerCase(); // Convert any letters to lower case
+
+			switch (input) {
+
+			case "1", "melee":
 				this.lvlMelee++;
-				System.out.println("You have upgraded your melee level. Your new melee level is: " + this.lvlMelee);
-				SkillPointUsed = true;
+				this.sklPt--;
 				break;
-			}
-			case "2", "ranged": {
+			case "2", "ranged":
 				this.lvlRanged++;
-				System.out.println("You have upgraded your ranged level. Your new ranged level is: " + this.lvlRanged);
-				SkillPointUsed = true;
+				this.sklPt--;
 				break;
-			}
-			case "3", "rogue": {
+			case "3", "rogue":
 				this.lvlRogue++;
-				System.out.println("You have upgraded your rogue level. Your new rogue level is: " + this.lvlRogue);
-				SkillPointUsed = true;
+				this.sklPt--;
 				break;
-			}
-			case "4", "mage": {
+			case "4", "mage":
 				this.lvlMage++;
-				System.out.println("You have upgraded your mage level. Your new mage level is: " + this.lvlMage);
-				SkillPointUsed = true;
+				this.sklPt--;
 				break;
-			}
-			case "5", "luck": {
+			case "5", "luck":
 				this.lvlLuck++;
-				System.out.println("You have upgraded your luck level. Your new luck level is: " + this.lvlLuck);
-				SkillPointUsed = true;
+				this.sklPt--;
 				break;
-			}
-			default: // Error message for improper format. Reprints choices and takes their choice
-						// again
-				System.out.println("Your inputted choice does not match any of the options or is not in the propper format. \n" + 
-									"Your choices are: \n" + 
-									"1) Melee Level: Your current Melee Level is " + this.lvlMelee + "\n" + 
-									"2) Ranged Level: Your current Ranged Level is " + this.lvlRanged + "\n" + 
-									"3) Rogue Level: Your current Rogue Level is " + this.lvlRogue + "\n" + 
-									"4) Mage Level: Your current Mage Level is " + this.lvlMage + "\n" + 
-									"5) Luck Level: Your current Luck Level is " + this.lvlLuck + "\n" + 
-									"Remember, you can either type the number or the class of your choice. Ex. '1' or 'Melee' \n"+ 
-									"What would you like to upgrade?");
+			case "6", "cancel": // Cancels spending and exits the method
+				return;
+			default:
+				System.out.println("Invalid Input\n");
+				break;
+
 			}
 		}
-		Keyboard.close();
+
+	}
+
+	/**
+	 * Void method to handle the player leveling up, and increase the exp needed to
+	 * level up. It also adds 1 skill point per level up for the player to use when
+	 * they are ready.
+	 */
+	public void lvlUp() {
+
+		if (this.exp < this.expThreshold) { // The player can't level up to begin with, so it exits the method
+			return;
+		}
+
+		int count = 0; // Counts the number of level ups in one call of this method
+
+		while (this.exp >= this.expThreshold) { // Loops as long as their exp is above the threshold
+			this.level++; // Increments level by one
+			this.sklPt++; // Adds a skill point on each loop.
+			this.exp -= (int) this.expThreshold; // Subtracts exp needed for current threshold
+			this.expThreshold *= 1.2; // Increase the threshold
+			count++;
+		}
+
+		if (count == 1) { // Only leveled up once in one sitting
+			System.out.println("Your level has increased to " + this.level + "! You've earned a skill point!");
+		} else { // Leveled up multiple times in one sitting
+			System.out.println(
+					"Your level has increased to " + this.level + "! You've earned " + count + " skill points!");
+		}
+
 	}
 
 	// -------------------toString-------------------
 
 	public String toString() {
-		return super.toString() + 
-				"Luck Level: " + this.lvlLuck + "\n" + 
-				"Mana: " + this.mana + "\n" +
-				"-----Class Levels-----\n" + 
-				"Melee Level: " + this.lvlMelee + "\n" + 
-				"Ranged Level: " + this.lvlRanged + "\n" + 
-				"Rogue Level: " + this.lvlRogue + "\n" + 
-				"Mage Level: " + this.lvlMage + "\n";
+		return super.toString() + "Luck Level: " + this.lvlLuck + "\n" + "Mana: " + this.mana + "\n"
+				+ "-----Class Levels-----\n" + "Melee Level: " + this.lvlMelee + "\n" + "Ranged Level: "
+				+ this.lvlRanged + "\n" + "Rogue Level: " + this.lvlRogue + "\n" + "Mage Level: " + this.lvlMage + "\n";
 	}
 
 }
