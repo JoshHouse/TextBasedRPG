84d0b31c160255db7eea258236a66885d35a2df8
Cameron Nickerson <nickersonc20@students.ecu.edu>
2024-11-05 19:39:58 +0000

    Adding in the melee tutorial enemy.

--- src/indexes/EnemyList.java
+++ src/indexes/EnemyList.java
@@ -2,10 +2,13 @@ package indexes;
 
 import game.Enemy;
 import game.Inventory;
-import game.SpecialAttack;
 
 public class EnemyList {
-	
+
 	public static Enemy sheep = new Enemy("Sheep", new Inventory(), 25, 50, 1, 10, 5, 1, SpAtkList.none);
+
+	/** Blacksmith Jagatai - Tutorial fight for melee class. */
+	public static Enemy tutor1 = new Enemy("Blacksmith Jagatai", new Inventory(), 0, 65, 1, 10, 10, 1,
+			SpAtkList.none);
 
 }
