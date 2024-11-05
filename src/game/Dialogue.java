04c96b5907a86a2282fcf1a534d5454db6287e71
Cameron Nickerson <nickersonc20@students.ecu.edu>
2024-11-05 22:45:46 +0000

    Update Dialogue.java

--- src/game/Dialogue.java
+++ src/game/Dialogue.java
@@ -1,30 +1,38 @@
-package game;
+	/*
+	 * Dialogue is printed using a for-loop that displays it one character at a
+	 * time, and the display time is controlled by a speed variable, given when the
+	 * methods are called.
+	 */
 
-public class Dialogue {
-	
-	private static Pause pause;
-	
-	public Dialogue() {
-		pause = new Pause();
-	}
-	
-	public static void characterDialogue(String name, String text) {
-		
+	/**
+	 * Prints the dialogue whenever a character is speaking, identifying them with
+	 * their name at the top of each dialogue printed.
+	 * 
+	 * @param name  - The character's name
+	 * @param text  - The dialogue to print
+	 * @param speed - The speed at which each letter is printed to the console
+	 */
+	public static void characterDialogue(String name, String text, long speed) {
+
 		System.out.println("[ " + name + " ]");
-		for (char ch: text.toCharArray()) {
+		for (char ch : text.toCharArray()) {
 			System.out.print(ch);
-			pause.pause(25);
+			Pause.pause(speed);
 		}
-		
 	}
-	
-	public static void infoDialogue(String text) {
-		
-		for (char ch: text.toCharArray()) {
+
+	/**
+	 * Prints regular dialogue that is not being spoken by any specific character.
+	 * 
+	 * @param text  - The dialogue to print
+	 * @param speed - The speed at which each letter is printed to the console
+	 */
+	public static void infoDialogue(String text, long speed) {
+
+		for (char ch : text.toCharArray()) {
 			System.out.print(ch);
-			pause.pause(25);
+			Pause.pause(speed);
 		}
-		
 	}
 
 }
