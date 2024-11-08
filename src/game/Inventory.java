1ba31289091bb31d8d5e74558aad2a37b9dc7aed
Joshua House <moderockgames@gmail.com>
2024-11-08 13:21:47 -0500

    Removed automatic head error text from git

--- src/game/Inventory.java
+++ src/game/Inventory.java
@@ -66,8 +66,6 @@ public class Inventory {
 		return weapons.get(0);
 	}
 	
-<<<<<<< HEAD
-=======
 	public Weapon getWeaponOnKey(String inputKey) {
 		for (int x = 0; x < weapons.size(); x++) {
 			if (inputKey.equals(weapons.get(x).getKey())) {
@@ -76,8 +74,6 @@ public class Inventory {
 		}
 		return null;
 	}
-	
->>>>>>> efdedd811bf12208204039c6b225635e3355138e
 	// ------------------------------Setters-------------------------------
 	
 	public void setWeapons(ArrayList<Weapon> inputWeapons) {
